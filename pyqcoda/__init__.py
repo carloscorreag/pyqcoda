7c749451cd8ef509cc677a0e8d70849fc0ee25b5
carloscorreag <103500086+carloscorreag@users.noreply.github.com>
2025-07-23 11:08:51 +0200

    Update __init__.py

--- pyqcoda/__init__.py
+++ pyqcoda/__init__.py
@@ -1,1 +1,13 @@
-from .core import pyqcoda
+"""
+pyqcoda: Temporal disaggregation of daily precipitation into hourly using Q-CODA.
+
+Main class:
+    - pyqcoda: Implements the disaggregation algorithm.
+"""
+
+from .core import pyqcoda  
+
+__all__ = ["pyqcoda"]
+__version__ = "1.0.0"
+
+
